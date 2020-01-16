de029eb7fc7acd42b123cf1be91f9afb063c378c
black753 <mauroo_00@hotmail.com>
2020-01-16 15:06:08 -0300

    Finalizacion
    Cree un nuevo archivo para el testeo de cypress porque el anterior se estaba volviendo casi ilegible. En este nuevo archivo ya esta el test completamente desarrollado y de forma ordenada

---
+++ cypress/integration/memotes2.js
@@ -0,0 +1,156 @@
+// Test completos del programa con cypress
+/// <reference types="Cypress" />
+
+const URL="192.168.1.37:8080"
+const NUMERO_CUADROS=16
+let diccionario_cartas={
+	banana:[],
+	manzana:[],
+	naranja:[],
+	uva:[],
+	anana:[],
+	frutilla:[],
+	pera:[],
+	sandia:[],
+}
+
+
+context("memotest", () => {
+
+	before(() => {
+
+		
+		cy.visit(URL)
+		
+
+	})
+
+
+	it("Verificando que haya un tablero con cartas", () => {
+		cy.get("#tablero").find("img").should("have.length", NUMERO_CUADROS)
+	})
+
+	it("Verificando la aleatoriedad de los cuadros",() => {
+		let primera_vuelta=[]
+		let segunda_vuelta=[]
+
+		cy.get("#btn-comenzar").click()
+
+		cy.get("img").then(imagenes => {
+			imagenes.each((i,e) => {
+				e.click()
+				primera_vuelta.push(e.src)
+			})
+		})
+
+
+		cy.visit(URL)
+		
+
+		cy.get("img").then(imagenes => {
+			imagenes.each((i,e) => {
+				e.click()
+				segunda_vuelta.push(e.src)
+			})
+		})
+
+		cy.wrap(primera_vuelta).should('not.deep.equal', segunda_vuelta)
+    })
+
+    describe("Resuelve el juego 1ra vez", () => {
+        it("obteniendo datos", ()=>{
+
+            cy.get("#btn-comenzar").click()
+            
+            cy.get("img").then(imagenes => {
+
+                imagenes.each((i,e) => {
+                    cy.get(e).click().then(carta => {
+        
+                        agrupar_cartas(carta.selector)
+                    })
+                })
+            })    
+
+        })
+
+        it("resolviendo", ()=>{
+            Object.values(diccionario_cartas).forEach(e => {					
+                cy.get(e[0]).click()
+                cy.get(e[1]).click()
+            })
+
+            cy.get("#cuenta-regresiva").should('have.text','Felicitaciones, has ganado!').then(()=>{
+                vaciarDiccionario()
+                
+            })
+        })
+
+
+    })
+
+    describe('Reiniciando y resolviendo de nuevo', ()=>{
+        it("obteniendo datos", ()=>{
+
+            cy.get("#btn-comenzar").click()
+            
+            cy.get("img").then(imagenes => {
+
+                imagenes.each((i,e) => {
+                    cy.get(e).click().then(carta => {
+        
+                        agrupar_cartas(carta.selector)
+                    })
+                })
+            })    
+
+        })
+
+        it('resolviendo', ()=>{
+            Object.values(diccionario_cartas).forEach(e => {					
+                cy.get(e[0]).click()
+                cy.get(e[1]).click()
+            })
+
+            cy.get("#cuenta-regresiva").should('have.text','Felicitaciones, has ganado!')
+
+        })
+    })
+
+
+})
+
+function agrupar_cartas(e){
+
+	if(((e.src).search("banana"))!== -1){
+		diccionario_cartas.banana.push(e)
+	}else if(((e.src).search("manzana"))!== -1){
+		diccionario_cartas.manzana.push(e)
+	}else if(((e.src).search("naranja"))!== -1){
+		diccionario_cartas.naranja.push(e)
+	}else if(((e.src).search("uva"))!== -1){
+		diccionario_cartas.uva.push(e)
+	}else if(((e.src).search("anana"))!== -1){
+		diccionario_cartas.anana.push(e)
+	}else if(((e.src).search("frutilla"))!== -1){
+		diccionario_cartas.frutilla.push(e)
+	}else if(((e.src).search("pera"))!== -1){
+		diccionario_cartas.pera.push(e)
+	}else if(((e.src).search("sandia"))!== -1){
+		diccionario_cartas.sandia.push(e)
+	}
+	
+}
+
+function vaciarDiccionario(){
+    diccionario_cartas={
+        banana:[],
+        manzana:[],
+        naranja:[],
+        uva:[],
+        anana:[],
+        frutilla:[],
+        pera:[],
+        sandia:[],
+    }
+}
